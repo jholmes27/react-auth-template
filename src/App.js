46709f4ab15b6df8c66dd5595b6ccdbc5842d0fb
jholmes <Josiah_Holmes@discovery.com>
2023-02-15 09:10:18 -0500

    Add Okta auth logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
+import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import Login from './components/Login';
+import Locked from './components/Locked';
+import { oktaConfig } from './lib/oktaConfig';
 
-function App() {
+
+const CALLBACK_PATH = `${process.env.REACT_APP_CALLBACK_PATH}`;
+
+const oktaAuth = new OktaAuth(oktaConfig);
+
+const Root = () => {
+  const history = useHistory();
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+        <Switch>
+          <Route path='/' exact={true} component={Login} />
+          <Route path={CALLBACK_PATH} exact={true} component={LoginCallback} />
+          <SecureRoute path='/locked' component={Locked} />
+        </Switch>
+    </Security>
   );
-}
+};
 
-export default App;
+
+export default function App() {
+  return (
+    <Router>
+      <Root />
+    </Router>
+  )
+}
